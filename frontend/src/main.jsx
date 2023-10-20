aaa7428a3ab3b363cad05a019920aff5872dbdef
AENIMA <aenima@AENIMAs-MacBook-Pro.local>
2023-10-20 13:27:36 +0200

    register,login,profile and logout added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,11 +7,15 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
+import store from './store.js';
+import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
+import ProfileScreen from './screens/ProfileScreen.jsx';
+import PrivateRoute from './components/PrivateRoute.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,12 +23,17 @@ const router = createBrowserRouter(
       <Route path='/' element={<HomeScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/profile' element={<ProfileScreen />} />
+      </Route>
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
